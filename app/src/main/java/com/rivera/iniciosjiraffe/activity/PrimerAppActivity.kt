4e0552987930b97894dc3aee287cbcdd2a2aec2d
irvinzato <multizato@hotmail.com>
2024-01-21 01:33:40 -0600

    Se añadio boton y comunicación con appActivity

--- app/src/main/java/com/rivera/iniciosjiraffe/activity/PrimerAppActivity.kt
+++ app/src/main/java/com/rivera/iniciosjiraffe/activity/PrimerAppActivity.kt
@@ -1,15 +1,31 @@
 package com.rivera.iniciosjiraffe.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.rivera.iniciosjiraffe.R
 
 class PrimerAppActivity : AppCompatActivity() {
+    //Al arrancar la pantalla
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i("Consola: ", "Primer mensaje")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_primer_app)
-        //Al arrancar la pantalla
+        val botonLoguea = findViewById<AppCompatButton>(R.id.botonLoguea)
+        val editTextMensaje = findViewById<AppCompatEditText>(R.id.editTextMensaje)
+
+
+        botonLoguea.setOnClickListener {
+            val editTextLog = editTextMensaje.text.toString()
+            if(editTextLog.isNotEmpty()) {
+                Log.i("Irving", "!Rendirse Jamas! $editTextLog")
+            }
+
+        }
+
     }
 }
